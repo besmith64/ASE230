259d0e1f822a83d55b8d06f9da6b4c91802574a1
Benjamin Smith <darkcreed64@BenjamisMacBook.lan1>
2022-09-11 23:16:31 -0400

    Fixed issue with the progress bar

--- details.php
+++ details.php
@@ -1,59 +1,71 @@
 <?php
-    //Declare Variables
-    //Multidimensional Array
-    $students = array(
-        array('name' =>'Ben Smith',
-              'designation' => 'Technical Systems Analyst',
-              'image' => 'img/me.jpg',
-              'facebook' => 'https://www.facebook.com/DarkCreed64',
-              'linkedin' => 'https://www.linkedin.com/in/benjamin-smith-27b867101/',
-              'level' => '<i class="fa-solid fa-4"></i>',
-              'profession' => 'Developer/System Administrator',
-              'company' => 'Microsoft',
-              'email' => 'smithb77@mymail.nku.edu',
-              'intro' => 'Hello! My name is Ben, I am a Computer Information Technology major with a minor in Information Security. 
+//Declare Variables
+//Multidimensional Array
+$students = array(
+    array(
+        'name' => 'Ben Smith',
+        'designation' => 'Technical Systems Analyst',
+        'image' => 'img/me.jpg',
+        'facebook' => 'https://www.facebook.com/DarkCreed64',
+        'linkedin' => 'https://www.linkedin.com/in/benjamin-smith-27b867101/',
+        'level' => '<i class="fa-solid fa-4"></i>',
+        'profession' => 'Developer/System Administrator',
+        'company' => 'Microsoft',
+        'email' => 'smithb77@mymail.nku.edu',
+        'intro' => 'Hello! My name is Ben, I am a Computer Information Technology major with a minor in Information Security. 
                           This is my senior year at NKU and previously I graduated from Cincinnati State with an Associates of Applied Science with my field of
                           study being Business Programming and Administration. I also work at Greater Cincinnati Water Works as a Technical Systems Analyst, where 
                           I oversee some of our custom web applications as well as data analysis and reporting. I have two dogs and two cats and enjoy spending time
                         with my wife and her family as well as being an avid gamer and tech enthusiast.',
-              'quote' => 'For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.',
-              'funfact' => "A fun fact about me is that I enjoy cooking and learning new recipes, if I didn't persue a career in Information Technology then I would 
+        'quote' => 'For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.',
+        'funfact' => "A fun fact about me is that I enjoy cooking and learning new recipes, if I didn't persue a career in Information Technology then I would 
                             have like to have become a chef and have my own food truck.",
-               'skills' => array('Data Analysis' => '85',
-                                 'Development' => '70',
-                                 'Data Administration' => '60')),
-        array('name' => 'David Price',
-              'designation' => 'System Administrator',
-              'image' => 'img/david_price.jpg',
-              'facebook' => 'https://www.facebook.com/zuck',
-              'linkedin' => 'https://www.linkedin.com/',
-              'level' => '<i class="fa-solid fa-2"></i>',
-              'profession' => 'IT Manager',
-              'company' => 'General Electric',
-              'email' => 'dprice@gmail.com',
-              'intro' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere praesentium debitis ipsa, eaque beatae delectus. Tenetur, placeat! Eaque aspernatur omnis magnam nam error exercitationem atque cum nisi dolorum, incidunt eveniet dicta facere quas totam adipisci dolorem odio? Minima dolore rem iure facere beatae nesciunt iste, fugit, recusandae quam non hic?',
-              'quote' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, numquam?',
-              'funfact' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nobis cumque ex omnis facere minus quam excepturi asperiores qui expedita?',
-              'skills' => array('Management' => '90',
-                                'System Administration' => '70',
-                                'Accounting' => '60')),
-        array('name' => 'Devon Nash',
-              'designation' => 'UX Designer',
-              'image' => 'img/devon_nash.jpg',
-              'facebook' => 'https://www.facebook.com/TheElonmusk',
-              'linkedin' => 'https://www.linkedin.com/',
-              'level' => '<i class="fa-solid fa-3"></i>',
-              'profession' => 'Game Developer',
-              'company' => 'Riot Games',
-              'email' => 'cool.dev@gmail.com',
-              'intro' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quo suscipit officiis vero minima nesciunt, provident temporibus perspiciatis, odio esse ea. Eum quidem numquam sint nesciunt consequatur ex modi autem aspernatur ullam at. Error porro, deleniti officiis dolorem animi ex ducimus ad veniam iusto ea, obcaecati repellendus! Optio, facilis laudantium?',
-              'quote' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. In, fuga?',
-              'funfact' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quis soluta optio dolores voluptates. Eum architecto quaerat excepturi repudiandae odit.',
-              'skills' => array('Graphic Design' => '80',
-                                'Development' => '50',
-                                'Team Building' => '60'))
-            );
-    
+        'skills' => array(
+            'Data Analysis' => '85',
+            'Development' => '70',
+            'Data Administration' => '60'
+        )
+    ),
+    array(
+        'name' => 'David Price',
+        'designation' => 'System Administrator',
+        'image' => 'img/david_price.jpg',
+        'facebook' => 'https://www.facebook.com/zuck',
+        'linkedin' => 'https://www.linkedin.com/',
+        'level' => '<i class="fa-solid fa-2"></i>',
+        'profession' => 'IT Manager',
+        'company' => 'General Electric',
+        'email' => 'dprice@gmail.com',
+        'intro' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere praesentium debitis ipsa, eaque beatae delectus. Tenetur, placeat! Eaque aspernatur omnis magnam nam error exercitationem atque cum nisi dolorum, incidunt eveniet dicta facere quas totam adipisci dolorem odio? Minima dolore rem iure facere beatae nesciunt iste, fugit, recusandae quam non hic?',
+        'quote' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, numquam?',
+        'funfact' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nobis cumque ex omnis facere minus quam excepturi asperiores qui expedita?',
+        'skills' => array(
+            'Management' => '90',
+            'System Administration' => '70',
+            'Accounting' => '60'
+        )
+    ),
+    array(
+        'name' => 'Devon Nash',
+        'designation' => 'UX Designer',
+        'image' => 'img/devon_nash.jpg',
+        'facebook' => 'https://www.facebook.com/TheElonmusk',
+        'linkedin' => 'https://www.linkedin.com/',
+        'level' => '<i class="fa-solid fa-3"></i>',
+        'profession' => 'Game Developer',
+        'company' => 'Riot Games',
+        'email' => 'cool.dev@gmail.com',
+        'intro' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quo suscipit officiis vero minima nesciunt, provident temporibus perspiciatis, odio esse ea. Eum quidem numquam sint nesciunt consequatur ex modi autem aspernatur ullam at. Error porro, deleniti officiis dolorem animi ex ducimus ad veniam iusto ea, obcaecati repellendus! Optio, facilis laudantium?',
+        'quote' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. In, fuga?',
+        'funfact' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quis soluta optio dolores voluptates. Eum architecto quaerat excepturi repudiandae odit.',
+        'skills' => array(
+            'Graphic Design' => '80',
+            'Development' => '50',
+            'Team Building' => '60'
+        )
+    )
+);
+
 ?>
 
 <!DOCTYPE html>
@@ -69,7 +81,7 @@
         integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/detail.css" />
     <title>
-        <?= 'ASE 230 - '.$students[$_GET['id']]['name']; ?>
+        <?= 'ASE 230 - ' . $students[$_GET['id']]['name']; ?>
     </title>
 </head>
 
@@ -81,7 +93,7 @@
             </nav>
         </div>
         <div class="row text-center">
-            <h1><?= 'This is ASE 230 - '.$students[$_GET['id']]['name']; ?></h1>
+            <h1><?= 'This is ASE 230 - ' . $students[$_GET['id']]['name']; ?></h1>
         </div>
     </div>
     <div class="container">
@@ -136,16 +148,16 @@
                 <div class="mb-2 mt-2 pt-1">
                     <h5 class="font-weight-normal">Top skills</h5>
                 </div>
-                <?php foreach($students[$_GET['id']]['skills'] as $name => $skill): ?>
+                <?php foreach ($students[$_GET['id']]['skills'] as $name => $skill) : ?>
                 <div class="py-1">
                     <div class="progress">
-                        <div class="progress-bar" role="progressbar" style=<?= "width: ".$skill."%"; ?>
+                        <div class="progress-bar" role="progressbar" style=<?= "'width: " . $skill . "%'"; ?>
                             aria-valuenow=<?= $skill; ?> aria-valuemin="0" aria-valuemax="100">
                             <div class="progress-bar-title">
                                 <?= $name; ?>
                             </div>
                             <span class="progress-bar-number">
-                                <?= $skill."%"; ?>
+                                <?= $skill . "%"; ?>
                             </span>
                         </div>
                     </div>
