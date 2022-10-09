9be1a319fe4c95f01fff1c597bb1f5ffb8f883b8
besmith64 <112007522+besmith64@users.noreply.github.com>
2022-10-09 02:05:04 -0400

    second revision

--- Great_Quotes_Proj/create.php
+++ Great_Quotes_Proj/create.php
@@ -1,3 +1,72 @@
 <?php
+include('csv_util.php');
 // the file displays a form with a text field where users can type the quote and a select box that displays all the available authors
 // as the form gets submitted, a new quote is added to quotes.csv
+$author = array(
+    1 => "Edgar Allen Poe",
+    2 => "Dr. Seus",
+    3 => "Walt Disney"
+);
+$quote = '';
+$error = '';
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['author'])) {
+        $error .= '<div class="alert alert-danger" role="alert">Please select an author.</div>';
+    }
+    if (empty($_POST['quote'])) {
+        $error .= '<div class="alert alert-danger" role="alert">Please enter a quote.</div>';
+    }
+    if ($error == '') {
+        $file = 'data\quotes.csv';
+        $values = array(
+            'author' => $_POST['author'],
+            'quote' => $_POST['quote']
+        );
+        write_csv($file, $values);
+        $error = '<div class="alert alert-success" role="alert">Successfully Submitted!</div>';
+        $p_author = '';
+        $p_quote = '';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Great Quotes!</title>
+</head>
+
+<body>
+    <div id="liveAlertPlaceholder"><?= $error ?></div>
+    <form method="POST">
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect">Author</label>
+            <select name="author" class="form-select" id="inputGroupSelect">
+                <option selected>Choose...</option>
+                <?php foreach ($author as $key => $val) : ?>
+                <option value=<?= $key; ?>><?= $val; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="input-group">
+            <span class="input-group-text">Add Quote</span>
+            <textarea name="quote" class="form-control" placeholder="Enter Quote"
+                aria-label="Add Quote"><?php echo $quote; ?></textarea>
+        </div>
+        <br></br>
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- Great_Quotes_Proj/csv_util.php
+++ Great_Quotes_Proj/csv_util.php
@@ -3,11 +3,11 @@
 function read_csv($file)
 {
     if (file_exists($file) and is_file($file)) { // Check if file exists before proceeding
-        $read_file = fopen($file, 'r'); // open the file
-        while (!feof($file)) {
-            $csv_array[] = fgetcsv($file); // append csv to array
+        $handle = fopen($file, 'r'); // open the file
+        while (($row = fgetcsv($handle, 1024)) !== false) {
+            $csv_array[] = $row; // append csv to array
         }
-        fclose($file); // close the file
+        fclose($handle); // close the file
     } else {
         die("Error: The file does not exist."); // return error if not a file
     }
@@ -16,29 +16,34 @@ function read_csv($file)
 // one function for reading the content of a CSV-formatted file into a PHP array and returning one element with index $element (i.e., only one record )
 function read_one_csv_element($file, $element)
 {
-    $f = read_csv($file); // pass to read_csv function to get array
-    foreach ($f as $key => $val) {
-        if ($key == $element) { // match the value to the index
-            return $val; // return the single value
+    $handle = read_csv($file); // pass to read_csv function to get array
+    foreach ($handle as $key => $val) {
+        if ($val[0] == $element) { // match the value to the index
+            return $val[1]; // return the single value
         }
     }
 }
 // one function for adding a new record to a CSV-formatted file
 function write_csv($file, $values)
 {
-    if (file_exists($file) and is_file($file)) {
-        $f = fopen($file, 'w');
-        foreach ($values as $line) {
-            fputcsv($f, $line);
-        }
+    if (file_exists($file) and is_file($file)) { // check to make sure file is being passed
+        $handle = fopen($file, 'a+'); // open file in write modee
 
-        fclose($f);
+        fputcsv($handle, $values); // add new row to csv file
+
+        // file_put_contents($f, $values);
+
+        fclose($handle);
     } else {
         die("Error: The file does not exist."); // return error if not a file
     }
 }
 // one function for modifying the record on a specific line
+function edit_csv($file, $values)
+{
+    $handle = read_csv($file); // pass to read_csv function to get array
 
+}
 // one function for emptying the record on a specific line (delete the content of a line, but leave an empty line  in the file)
 
 // one function for deleting a line from the file (delete the line altogether)
